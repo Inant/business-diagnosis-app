7edef4b923768f5cf3750ff04cceaefb0665b5c3
Inant Kharisma <inant.kharisma@gmail.com>
2025-07-26 21:26:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Backoffice\QuestionController;
 use App\Http\Controllers\Frontoffice\FormController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
